a897385651672d6495652fe5c928fb939de550b1
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-01-27 11:50:48 +0100

    Add List of waiting queue

--- src/store.ts
+++ src/store.ts
@@ -3,19 +3,31 @@ import Vuex from 'vuex';
 import Router from './router';
 import Queue from '@/models/Queue';
 import Api from '@/lib/Api';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    queues: []
   },
   mutations: {
+    setQueues(state, queues) {
+      state.queues = queues;
+    },
     addQueue(state, queue: Queue) {
+      state.queues.push(queue);
       Router.push({ name: 'numberselect' });
     },
   },
   actions: {
+    init({commit}) {
+      Api
+        .get(`queues`)
+        .then((response: any) => {
+          commit('setQueues', response.data['hydra:member']);
+        });
+    },
     flash({ commit }, uid) {
       Api
         .patch(`queues/${uid}/state`, {
@@ -25,5 +37,28 @@ export default new Vuex.Store({
           commit('addQueue', response);
         });
     },
+    ready({ commit }, item) {
+      Api
+        .patch(`queues/${item.customerId}/state`, {
+          state: 'ready',
+        })
+        .then((response: any) => {
+          this.dispatch('init');
+        });
+    },
+    reset({ commit }, item) {
+      Api
+        .patch(`queues/${item.customerId}/state`, {
+          state: 'reset',
+        })
+        .then((response: any) => {
+          this.dispatch('init');
+        });
+    },
   },
+  plugins: [
+    createPersistedState({
+      key: 'takeMEATable-holder',
+    }),
+  ],
 });
--- src/views/SelectNumber.vue
+++ src/views/SelectNumber.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="selectNumber">
-    <h1>HERE</h1>
-  </div>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-list-tile v-for="item in $store.state.queues" :key="item.customerId" v-if="item.waiting">
+        <v-list-tile-avatar>
+           <v-icon color="teal" @click="$store.dispatch('ready', item)">add_alert</v-icon>
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title v-html="item.customerId"></v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+            <v-icon color="red" @click="$store.dispatch('reset', item)">remove_circle</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class SelectNumber extends Vue {}
+export default class SelectNumber extends Vue {
+  public created() {
+    this.$store.dispatch('init');
+  }
+}
 </script>
